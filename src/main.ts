edbc870f90b753725a030e5baca4a55d843d0306
Steven Xie <dev@stevenxie.me>
2020-11-02 18:58:00 -0500

    Release initial version

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,6 @@ import { debug, getInput, setOutput, setFailed } from "@actions/core"
 import { getPushTags } from "./push"
 import { getPullRequestTags } from "./pull-request"
 
-async function run(): Promise<void> {
-  try {
-    const repo: string = getInput("repo")
-    const tags = getTags()
-    const output = tags.map(tag => `${repo}:${tag}`).concat(",")
-    setOutput("tags", output)
-  } catch (error) {
-    setFailed(error.message)
-  }
-}
-
 const getTags = (): string[] => {
   const eventName = process.env.GITHUB_EVENT_NAME
   debug(`Got event: ${eventName}`)
@@ -25,3 +14,16 @@ const getTags = (): string[] => {
       throw new Error("Unsupported event name.")
   }
 }
+
+function run(): void {
+  try {
+    const repo: string = getInput("repo")
+    const tags = getTags()
+    const output = tags.map(tag => `${repo}:${tag}`).join(",")
+    setOutput("tags", output)
+  } catch (error) {
+    setFailed(error.message)
+  }
+}
+
+run()
